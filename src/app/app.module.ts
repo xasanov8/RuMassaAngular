3bd1bf097e0f90f79ef6c17cffc192ca985c30d5
AbdullohDev07 <gamesupro01@gmail.com>
2024-05-12 19:02:03 +0500

    goo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,30 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './Pages/home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
+import { HomeInTwoComponent } from './Pages/home/home-in-two/home-in-two.component';
+import { HomeInOneComponent } from './Pages/home/home-in-one/home-in-one.component';
+import { LoginComponent } from './Pages/login/login.component';
+import { RegisterComponent } from './Pages/register/register.component';
+import { ErrorComponent } from './Pages/error/error.component';
+import { KontaktComponent } from './Pages/kontakt/kontakt.component';
+import { SpasiboComponent } from './Pages/spasibo/spasibo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    NavbarComponent,
+    FooterComponent,
+    HomeInTwoComponent,
+    HomeInOneComponent,
+    LoginComponent,
+    RegisterComponent,
+    ErrorComponent,
+    KontaktComponent,
+    SpasiboComponent,
   ],
   imports: [
     BrowserModule,
