b9763398d91903f1a4c42bfaa707f8f4254ccfe2
AbdullohDev07 <gamesupro01@gmail.com>
2024-05-21 10:31:00 +0500

    update files

--- src/app/Pages/novosti/novosti.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'app-novosti',
-  templateUrl: './novosti.component.html',
-  styleUrl: './novosti.component.scss'
-})
-export class NovostiComponent {
-}
