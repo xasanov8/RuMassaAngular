0090721d8e1101a7657bf2da440cb454d158c8a5
Abdulloh <118359840+xasanov8@users.noreply.github.com>
2024-05-21 10:14:56 +0500

    Revert "some changes"

--- src/app/Pages/novosti/novosti-one/novosti-one.component.ts
+++ src/app/Pages/novosti/novosti-one/novosti-one.component.ts
@@ -1,54 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { NovostiService } from '../novosti.service';
-import { CardModel } from '../../../interfaces/card-model';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-novosti-one',
   templateUrl: './novosti-one.component.html',
   styleUrl: './novosti-one.component.scss'
 })
-export class NovostiOneComponent implements OnInit {
-  
-  constructor(private novostiService: NovostiService) { }
-  news !: CardModel[];
-  cards: any[] = [];
-  pageIndex: number = 1;
-  size: number = 12;
+export class NovostiOneComponent {
 
-  ngOnInit(): void {
-    this.getAll(this.pageIndex, this.size);
-  }
-
-  previousPage(): void {
-    if (this.pageIndex >= 12){
-      this.pageIndex -= 12;
-    }
-    this.getAll(this.pageIndex, this.size);
-  }
-
-  nextPage(): void {
-    this.pageIndex += 12;
-    this.getAll(this.pageIndex, this.size);
-  }
-
-  getAll(pageIndex: number, size: number): void {
-    this.novostiService.getAllNews(pageIndex, size).subscribe(
-      (data: CardModel[]) => {
-        this.news = data;
-        this.updateCards();
-      }
-    );
-  }
-  
-  updateCards(): void {
-    this.cards = this.news.map(newsItem => ({
-      imgSrc: newsItem.cardPhoto,
-      category: 'Без рубрики',
-      title: newsItem.title,
-      date: newsItem.date,
-      authorImg: newsItem.user?.PhotoUrl || 'default-avatar.png',
-      authorName: newsItem.user?.Name || 'Unknown Author',
-      content: newsItem.description
-    }));
-  }
 }
